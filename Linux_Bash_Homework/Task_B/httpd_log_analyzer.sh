9c9ac040811392c459d504ac31f11ff206d33924
Dmytro Vozniuk <plibnik@you-know-what-google.com>
2023-01-31 00:05:17 +0000

    Task 2, functions 1,2

--- Linux_Bash_Homework/Task_B/httpd_log_analyzer.sh
+++ Linux_Bash_Homework/Task_B/httpd_log_analyzer.sh
@@ -13,9 +13,25 @@ function process_log () {
 		echo "File $1 does not exist. Terminating..."
 		exit 3
 	fi
+
+	FILENAME=$1
+
+	echo "1. From which ip there were most requests?"
+	echo -n "Most popular request IP: "
+	cut -f 1 -d ' ' < $FILENAME | uniq -c | sort -nr | head -n 1 | cut -c 9-
+
+
+	echo -e "\n2. What is the most requested page?"
+	echo -n "Most requested page:"
+	grep '"GET ' < $FILENAME | cut -d ' ' -f 7 | sort | uniq -c | sort -nr | head -n 1 | cut -c 9-
+# We filter only GET requests, not POST!
+
+
+
 }
 
 
+# Begin of main code
 
 echo "Apache Log Analyzer script v 0.01"
 echo "Number of parameters: $#"
