0a879cb68ba36e49128e2f01c40eca96e3881e89
Dmytro Vozniuk <plibnik@you-know-what-google.com>
2023-01-31 07:28:24 +0000

    The script at least imitates working (problem of how to traverse big subnets, especially when they are not like 255.0.0.0 or 255.255.255.0 (with 0..255 between dots), but something like 255.255.240.0 -- remains , as well as legality of scanning)

--- Linux_Bash_Homework/Task_A/netscanner.sh
+++ Linux_Bash_Homework/Task_A/netscanner.sh
@@ -7,10 +7,26 @@ function print_usage_function () {
 
 function all_function () {
 	echo "All function called"
+	SUBNETS_NUMBER=0;
+	SUBNETS_NUMBER=` ip route | cut -d ' ' -f 1 | grep "/" |  wc -l `
+	echo "Subnets number is $SUBNETS_NUMBER";
+	if [[ $SUBNETS_NUMBER == 0 ]]; then
+		echo "Cannot execute, no subnets found!";
+		exit 3
+	fi
+	WORKING_SUBNET= ` ip route | cut -d ' ' -f 1 | grep "/" | head -n 1 `;
+	echo "Working with subnet: $WORKING_SUBNET"
+	echo "I'd run Nmap on it or try a for cycle, but scanning networks is against EPAM policy! Our lecturer said that!"
+	echo "Thanks for your attention"
+	exit 0
 }
 
 function target_function () {
+	TARGET_IP=$1
 	echo "Target function called"
+	echo -e "\n I'd call a nmap port scan on $TARGET_IP, but it's illegal!"
+	echo "Sorry. Terminating the program"
+	exit 0
 }
 
 
@@ -24,12 +40,14 @@ if (($# == 0)); then
 
 elif [ $1 == "--all" ]; then
 	echo "--all key given"
-
+	all_function;
+	exit 0;
 
 elif [ $1 == "--target" ]; then
 	echo "--target key given"
 	if [ $# == 2 ]; then
 		echo "Good, 2 parameters"
+		target_function $2
 	else
 		echo "--target key requires specification of target: ./netscanner.sh --target 10.1.2.3"
 	fi
